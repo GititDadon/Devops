180508c20a72fa03d25862c75dd0c7581e7ba3d3
GititDadon <92985984+GititDadon@users.noreply.github.com>
2023-06-04 16:31:38 +0300

    added route grades

--- src/app.js
+++ src/app.js
@@ -46,6 +46,30 @@ app.post('/register', async (req, res) => {
     res.status(400).json({ success: false, error: err.message });
   }
 });
+// Route to retrieve grades
+app.get('/api/grades', async (req, res) => {
+  try {
+    // Retrieve the grades from the database
+    const user = await User.findOne({ /* Add conditions if needed */ });
+    
+    // Check if the user and grades exist
+    if (!user || !user.grade1 || !user.grade2 || !user.grade3) {
+      return res.status(404).json({ success: false, message: 'Grades not found' });
+    }
+    
+    // Construct the grades object
+    const grades = {
+      grade1: user.grade1,
+      grade2: user.grade2,
+      grade3: user.grade3
+    };
+    
+    // Send the grades as a JSON response
+    res.json({ success: true, data: grades });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);})
