ed6c13e4b3b45bd97cd792f82d16d115a0f6f7e4
GititDadon <92985984+GititDadon@users.noreply.github.com>
2023-06-04 12:18:16 +0300

    changes app again

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ var path = require('path');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
