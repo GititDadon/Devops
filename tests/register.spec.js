4b1b5260649cb2ed7c40552c1a91859e92763d04
GititDadon <92985984+GititDadon@users.noreply.github.com>
2023-06-05 14:00:46 +0300

    go back to the old commit

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -1,57 +1,57 @@
-// const request = require('supertest');
-// const { app } = require('../src/app');
+const request = require('supertest');
+const { app } = require('../src/app');
 
-// describe('Test suite 1:', () => {
-//   test('test 1:', async () => {
-//     const res = await request(app).get('/');
-//     expect(res.statusCode).toEqual(200);
-//   });
+describe('Test suite 1:', () => {
+  test('test 1:', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
 
-//   test('test 2:', async () => {
-//     const res = await request(app).get('/1234');
-//     expect(res.statusCode).toEqual(404);
-//   });
-// });
+  test('test 2:', async () => {
+    const res = await request(app).get('/1234');
+    expect(res.statusCode).toEqual(404);
+  });
+});
 
-// describe('Test suite 2:', () => {
-//   let server;
+describe('Test suite 2:', () => {
+  let server;
 
-//   beforeAll((done) => {
-//     server = app.listen(0, () => {
-//       const port = server.address().port;
-//       process.env.PORT = port.toString();
-//       done();
-//     });
-//   });
+  beforeAll((done) => {
+    server = app.listen(0, () => {
+      const port = server.address().port;
+      process.env.PORT = port.toString();
+      done();
+    });
+  });
 
-//   afterAll((done) => {
-//     server.close(done);
-//   });
-//   var successCode=302;
-//   test('test 2: Register user', async () => {
-//     const userData = {
-//       name: 'Gitit Dadon',
-//       id: '123456789',
-//       grade1: 99,
-//       grade2: 90,
-//       grade3: 95,
-//     };
+  afterAll((done) => {
+    server.close(done);
+  });
+  var successCode=302;
+  test('test 2: Register user', async () => {
+    const userData = {
+      name: 'Gitit Dadon',
+      id: '123456789',
+      grade1: 99,
+      grade2: 90,
+      grade3: 95,
+    };
 
-//     const res = await request(app).post('/register').send(userData);
-//     console.log(userData);
-//     expect(res.statusCode).toEqual(successCode);
-//   });
+    const res = await request(app).post('/register').send(userData);
+    console.log(userData);
+    expect(res.statusCode).toEqual(successCode);
+  });
 
-//   test('test 3: Invalid user registration', async () => {
-//     const userData = {
-//       name: 'Gitit667Dadon',
-//       id: '123456789!!!',
-//       grade1: 990,
-//       grade2: 90,
-//       grade3: -95,
-//     };
+  test('test 3: Invalid user registration', async () => {
+    const userData = {
+      name: 'Gitit667Dadon',
+      id: '123456789!!!',
+      grade1: 990,
+      grade2: 90,
+      grade3: -95,
+    };
 
-//     const res = await request(app).post('/register').send(userData);
-//     expect(res.statusCode).toEqual(400);
-//   });
-// });
+    const res = await request(app).post('/register').send(userData);
+    expect(res.statusCode).toEqual(400);
+  });
+});
