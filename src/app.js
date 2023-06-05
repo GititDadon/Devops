0faabbdaa814f6523cafb570cb420fab990b2df6
GititDadon <92985984+GititDadon@users.noreply.github.com>
2023-06-05 13:34:17 +0300

    try8

--- src/app.js
+++ src/app.js
@@ -72,7 +72,9 @@ app.get('/api/grades', async (req, res) => {
     }
 
     // Extract the grades from the user object
-    const { grade1, grade2, grade3 } = user.grades;
+    const  grade1=user.grade1;
+    const grade2=user.grade2
+    const  grade3= user.grade3;
 
     // Send the grades as a JSON response
     res.json({ success: true, data: { grade1, grade2, grade3 } });
